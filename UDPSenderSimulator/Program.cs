0a42677ebc0c24b820dfb74e09c10502f0f47efc
pgr4 <iampgr4@gmail.com>
2014-09-21 20:28:27 -0400

    Added TryCatch

--- UDPSenderSimulator/Program.cs
+++ UDPSenderSimulator/Program.cs
@@ -12,55 +12,64 @@ namespace UDPSenderSimulator
     {
         static void Main(string[] args)
         {
-            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            try
+            {
+                Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
 
-            IPAddress broadcast = IPAddress.Parse("192.168.1.255");
+                IPAddress broadcast = IPAddress.Parse("192.168.1.255");
 
-            IPEndPoint ep = new IPEndPoint(broadcast, 30003);
+                IPEndPoint ep = new IPEndPoint(broadcast, 30003);
 
-            //Sender IP
-            byte[] sourceIP = new byte[4];
-            sourceIP[0] = 192;
-            sourceIP[1] = 168;
-            sourceIP[2] = 1;
-            sourceIP[3] = 2;
+                //Sender IP
+                byte[] sourceIP = new byte[4];
+                sourceIP[0] = 192;
+                sourceIP[1] = 168;
+                sourceIP[2] = 1;
+                sourceIP[3] = 2;
 
-            //Destination IP
-            byte[] destinationIP = new byte[4];
-            destinationIP[0] = 192;
-            destinationIP[1] = 168;
-            destinationIP[2] = 1;
-            destinationIP[3] = 5;
+                //Destination IP
+                byte[] destinationIP = new byte[4];
+                destinationIP[0] = 192;
+                destinationIP[1] = 168;
+                destinationIP[2] = 1;
+                destinationIP[3] = 5;
 
-            //Command
-            byte command = 0x01;
+                //Command
+                byte command = 0x01;
 
-            //Signal end of Header Info
-            byte[] cutoffSequence = new byte[6];
-            cutoffSequence[0] = 111;
-            cutoffSequence[1] = 111;
-            cutoffSequence[2] = 111;
-            cutoffSequence[3] = 111;
-            cutoffSequence[4] = 111;
-            cutoffSequence[5] = 111;
+                //Signal end of Header Info
+                byte[] cutoffSequence = new byte[6];
+                cutoffSequence[0] = 111;
+                cutoffSequence[1] = 111;
+                cutoffSequence[2] = 111;
+                cutoffSequence[3] = 111;
+                cutoffSequence[4] = 111;
+                cutoffSequence[5] = 111;
 
-            //Message id (I can see this not being needed) It does however give us a way to show which songs came in together but this could be done elsewhere
-            //Amount of breaks
-            byte[] message = new byte[3];
-            message[0] = 0x88;  //id
-            message[1] = 0x89;  //id
-            message[2] = 5;     //breaks
+                //Message id (I can see this not being needed) It does however give us a way to show which songs came in together but this could be done elsewhere
+                //Amount of breaks
+                byte[] message = new byte[3];
+                message[0] = 0x88;  //id
+                message[1] = 0x89;  //id
+                message[2] = 5;     //breaks
 
-            byte[] SendArray = new byte[sourceIP.Length + destinationIP.Length + cutoffSequence.Length + message.Length + 1];
-            sourceIP.CopyTo(SendArray, 0);
-            destinationIP.CopyTo(SendArray, 4);
-            SendArray[8] = command;
-            cutoffSequence.CopyTo(SendArray, 9);
-            message.CopyTo(SendArray, 15);
+                byte[] SendArray = new byte[sourceIP.Length + destinationIP.Length + cutoffSequence.Length + message.Length + 1];
+                sourceIP.CopyTo(SendArray, 0);
+                destinationIP.CopyTo(SendArray, 4);
+                SendArray[8] = command;
+                cutoffSequence.CopyTo(SendArray, 9);
+                message.CopyTo(SendArray, 15);
 
-            s.SendTo(SendArray, ep);
+                s.SendTo(SendArray, ep);
 
-            Console.WriteLine("Message sent to the broadcast address");
+                Console.WriteLine("Message sent to the broadcast address");
+            }
+            catch(Exception e)
+            {
+                Console.Write(e.Message + "\n");
+                Console.Write(e.InnerException);
+                Console.Read();
+            }
         }
     }
 }
